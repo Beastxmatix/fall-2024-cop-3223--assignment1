33a12b8896cbebb899166d88056d4225a5f73349
Beastxmatix <bnieves022@gmail.com>
2024-09-07 16:55:30 -0400

    Create fracturing.c

---
+++ fracturing.c
@@ -0,0 +1,9 @@
+// Assignment.1-fracture
+// Author: Brandon Nieves
+// UCF ID: 5482386
+// Date: 9/7/24
+// Class: COP 3223, Professor Parra
+// Purpose: To learn how to fracture and split up code where it does not reside under the main function
+#include <stdio.h>
+//include the library for the scan and print functions\
+//create the main function that holds all the data/allows the function to run (without main it would not be able to run)
